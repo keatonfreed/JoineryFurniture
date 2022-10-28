8293e0cc277932ee1cbcde3439171d4211ecff70
Keaton Freed <keatonafreed2@icloud.com>
2022-10-27 23:04:02 -0700

    fixed topbar and added responsive

--- script.js
+++ script.js
@@ -1,2 +1,30 @@
 
-document.getElementById()
+navLinks = document.getElementById("navLinks")
+navExtra = document.querySelectorAll(".navExtra")
+
+
+navExtra.forEach((object) => {
+    object.addEventListener("click", (event) => {
+        console.log(event.target)
+    })
+})
+
+
+
+
+function resize(event) {
+    event = event || false
+
+    if (event) {
+
+    }
+
+
+}
+
+resize()
+
+
+window.addEventListener('resize', (event) => {
+    resize(event)
+});
