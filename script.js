d3d5b6d7830a735b9cccda2a05b80819a2372246
Keaton Freed <keatonafreed2@icloud.com>
2022-10-29 10:05:22 -0700

    added animated dropdown and more button that appears when its too small

--- script.js
+++ script.js
@@ -3,9 +3,41 @@ navLinks = document.getElementById("navLinks")
 navExtra = document.querySelectorAll(".navExtra")
 
 
-navExtra.forEach((object) => {
-    object.addEventListener("click", (event) => {
-        console.log(event.target)
+
+navExtra.forEach(object => {
+
+
+    const select = object.querySelector('.dropdownSelect');
+    const selected = object.querySelector('.dropdownSelect .dropdownTitle');
+    const caret = object.querySelector('.dropdownSelect .dropdownIcon');
+
+    const menu = object.querySelector('.dropdownContents');
+    const options = object.querySelectorAll('.dropdownContents .dropdownItem');
+
+
+    select.addEventListener('click', () => {
+        // select.classList.toggle('select')
+        caret.classList.toggle('dropdownIcon-active')
+        menu.classList.remove('menu-close');
+        if (menu.classList.contains('menu-open')) {
+            menu.classList.add('menu-close');
+        }
+        menu.classList.toggle('menu-open')
+
+        options.forEach(option => {
+            option.addEventListener('click', () => {
+                // select.classList.remove()
+                caret.classList.remove('dropdownIcon-active');
+                menu.classList.remove('menu-open');
+                menu.classList.add('menu-close');
+
+                option.forEach(option => {
+                    option.classList.remove('dropdownItem-active')
+                })
+
+                option.classList.add('dropdownItem-active')
+            })
+        })
     })
 })
 
